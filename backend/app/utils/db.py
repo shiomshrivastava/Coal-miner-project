6465138486ea558ea63a3a092093246b8a082f71
Shiom Shrivastava <shrivastav30575@gmail.com>
2026-04-26 16:26:03 +0530

    Update database code

--- backend/app/utils/db.py
+++ backend/app/utils/db.py
@@ -2,5 +2,5 @@ import psycopg2
 
 def get_db_connection():
     return psycopg2.connect(
-        "postgresql://postgres:Shiom_Trisha%404946@db.exydcpiaxeaxvwzsvntz.supabase.co:5432/postgres"
+        "postgresql://postgres.exydcpiaxeaxvwzsvntz:Shiom_Trisha%404946@aws-1-ap-south-1.pooler.supabase.com:6543/postgres"
     )
